52318f7ddb47b8971cb32e26a8dc15ba962fc372
DeAndre Boston <dboston3@yahoo.com>
2019-03-21 14:41:56 -0500

    Add constructor to Time component

--- frontend-react/src/components/Time.js
+++ frontend-react/src/components/Time.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react';
 
 class Time extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: null
+        };
+
+    };
     render() {
         return (
             <div>
                 <h2>Time</h2>
                 <form>
-                    <input type='number'></input>
+                    <input type='number' value={this.state.value}></input>
                 </form>
                 <select>
                     <option>Minutes</option>
