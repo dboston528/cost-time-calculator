f99d92703e63de0f15f14539e45e8314ea4d42e3
DeAndre Boston <dboston3@yahoo.com>
2019-03-28 12:56:00 -0500

    Update time component and add image

--- frontend-react/src/components/Time.js
+++ frontend-react/src/components/Time.js
@@ -3,16 +3,22 @@ import React, { Component } from 'react';
 class Time extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            value: null
-        };
+        this.state = { value: [] };
 
-    };
+
+        this.handleChange = this.handleChange.bind(this);
+
+    }
+
+    handleChange(event) {
+        this.setstate({ value: event.target.value });
+    }
+
     render() {
         return (
             <div>
                 <form >
-                    <input className="time-input" type='number' placeholder="Time" value={this.state.value}></input>
+                    <input className="time-input" type='text' placeholder="Time" value={this.state.value} ></input>
                 </form>
             </div>
         )
